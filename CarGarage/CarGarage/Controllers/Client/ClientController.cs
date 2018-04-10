8186c94d92219490a9526f4fdab9a5e4d8fe07bc
Ilies Mulot <ilies.mulot@epitech.eu>
2018-04-10 21:54:53 +0200

    Ajout client controller WIP

--- CarGarage/CarGarage/Controllers/Client/ClientController.cs
+++ CarGarage/CarGarage/Controllers/Client/ClientController.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿using CarGarage.Services;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace CarGarage.Controllers.Client
 {
-    public class ClientController : Controller
+    
+    public class ClientController : ApiController
     {
-        // GET: Client
-        public ActionResult Index()
+        ClientService _clientService;
+
+/*        ClientController(ClientService clientService)
         {
-            return View();
+            _clientService = clientService;
+        }*/
+
+        [HttpPost]
+        [Route("Client/Register")]
+        public async Task<IHttpActionResult> Register([FromBody] string payload)
+        {
+            //var client = JsonConvert.DeserializeObject(payload);
+
+            /*if (client != null)
+            {
+                if (await _clientService.Add())
+                    return Ok("Sucessfully registered");
+            }*/
+            return Ok(payload);
+            //return InternalServerError();
         }
     }
 }
